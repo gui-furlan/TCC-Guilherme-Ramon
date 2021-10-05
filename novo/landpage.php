85fbd941b840803bc9a12d98a57a6bffb5aceb55
gui-furlan <guilherme.furlan.nunes@gmail.com>
2021-10-05 20:34:13 -0300

    5/10/2021 20:34

---
+++ novo/landpage.php
@@ -0,0 +1,14 @@
+<!-- conteudoIndex.php -->
+
+<main class="index-main">
+    <section class="boas-vindas">
+        <div class="container">
+            <h1>Bem vindo!</h1>
+            <p>Preparado para encontrar sua próxima experiência profissional em tecnologia?</p>
+            <div class="index-conta-f"></div>
+            <div class="index-conta-j"></div>
+        </div>
+    </section>
+</main>
+
+<!-- Fim do conteudoIndex.php -->
