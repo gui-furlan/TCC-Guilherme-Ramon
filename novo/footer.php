c9b614af7378863b30b35e8c997f6afe0752b48a
gui-furlan <guilherme.furlan.nunes@gmail.com>
2021-10-05 23:22:07 -0300

    5/05/2021 23:21

---
+++ novo/footer.php
@@ -0,0 +1,16 @@
+
+<!-- footer.php -->
+
+<footer>
+    <div class="footer-conteudo">
+        <div class="container">
+            <p>Este é um projeto de trabalho de conclusão de curso técnico de informática integrado do IFC Campus Ibirama</p>
+            <p>Alunos:</p>
+            <p>Guilherme Furlan Nunes</p>
+            <p>Ramon José Pinto</p>
+        </div>
+    </div>
+</footer>
+</body>
+
+</html>
--- novo/index.php
+++ novo/index.php
@@ -2,3 +2,4 @@
 
 include "header.php";
 include "landpage.php";
+include "footer.php";
