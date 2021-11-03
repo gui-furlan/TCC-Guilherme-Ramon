ed6d820942c1d23a946ddd921962e0c35b1ed7ed
gui-furlan <guilherme.furlan.nunes@gmail.com>
2021-11-03 16:52:39 -0300

    Conexao alterada

--- conexao.php
+++ conexao.php
@@ -3,6 +3,6 @@
 $servername = "localhost";
 $username = "guilherme";
 $password = "abc123";
-$dbname = "tcc2";
+$database = "tcc2";
 
-$mysqli = new mysqli($servername, $username, $password, $dbname);
+$mysqli = new mysqli($servername, $username, $password, $database);
